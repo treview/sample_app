2bc5e238db734f7fc46a5c4f7f8d2bb426022096
Tom Gernon <tgernon@review.com>
2013-03-20 16:11:05 -0400

    Finish sign in 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
   private
     def create_remember_token
